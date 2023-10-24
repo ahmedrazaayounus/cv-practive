61199d716dcd246ff000070c65843d07815639a1
Ahmed raza <144343013+ahmedrazaayounus@users.noreply.github.com>
2023-10-24 04:41:46 -0700

    Add files via upload

---
+++ sctipt.js
@@ -0,0 +1,11 @@
+// const update1 = document.querySelector(".hidden1");
+// const update2 = document.querySelector(".hidden2");
+// const line = document.querySelector(".line");
+// const button = document.querySelector("button");
+
+
+// button.addEventListener("click", () => {
+//     update1.style.display = "block";
+//     update2.style.display = "block";
+//     line.style.height = "570px";
+// })
